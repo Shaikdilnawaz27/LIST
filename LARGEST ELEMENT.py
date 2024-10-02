10fa24444878a12e5ab5b23eef20581a30407fc4
Shaikdilnawaz <154993930+Shaikdilnawaz27@users.noreply.github.com>
2024-10-02 16:33:02 +0530

    Create LARGEST ELEMENT.py

---
+++ LARGEST ELEMENT.py
@@ -0,0 +1,24 @@
+'''
+Write a Python Program to find the largest number in a list
+Input & Output Format:
+Input consists of one list and one integer.
+First input consists of a size of a list.
+Second inputs corresponds to the size of the list.
+Output consists of the largest element.
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+6
+'''
+size = int(input())
+elements = []
+for _ in range(size):
+    element = int(input())
+    elements.append(element)
+largest_element = max(elements)
+print(largest_element)
